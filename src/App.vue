f5ee6a62ffbc3f3137442c38d95b1a553b2df286
nahid18 <abdullah.18@student.sust.edu>
2019-09-03 04:09:08 +0600

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo List</h1>
+    <Todos v-bind:todos="todos" />
+    <br/>
   </div>
+
+  
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+
+<script>
+  import Todos from './components/Todos.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      Todos,
+    },
+    data() {
+      return {
+        todos: [
+          {
+            id: 1,
+            title: "Listening",
+            isCompleted: false
+          },
+          {
+            id: 2,
+            title: "Reading",
+            isCompleted: false
+          },
+          {
+            id: 3,
+            title: "Sleeping",
+            isCompleted: true
+          },
+        ]
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    font-family: 'Roboto', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="Todos">
+
+      <div v-bind:key="i.id" v-for="i in todos">
+        <h3> {{i.title}} </h3>
+      </div>
+      
+    </div>
+</template>
+
+
+
+<script>
+export default {
+  name: "Todos",
+  props: ["todos"]
+}
+</script>
+
+
+
+<style scoped>
+  .Todos {
+    margin-top: 5%;
+  }
+</style>
